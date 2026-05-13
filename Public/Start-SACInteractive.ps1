d4e5294ab31945bc52e94c2c3a25e99e4849ee48
Dailen Gunter <me@dailen.net>
2026-05-13 16:41:20 -0400

    chore(version): bump to 2.3.4-beta for monitor fix

--- Public/Start-SACInteractive.ps1
+++ Public/Start-SACInteractive.ps1
@@ -396,7 +396,7 @@ $command
         $borderLine = $V + (" " * $W) + $V
 
         # Top border + title
-        $title     = "  SURGICAL AUTODESK CLEANER  v2.3.3-beta"
+        $title     = "  SURGICAL AUTODESK CLEANER  v2.3.4-beta"
         $titlePad  = $title.PadLeft(($W + $title.Length) / 2)
         Write-Host "$TL$border$TR" -ForegroundColor DarkCyan
         Write-BoxLine -Text $titlePad -Color "Cyan"
