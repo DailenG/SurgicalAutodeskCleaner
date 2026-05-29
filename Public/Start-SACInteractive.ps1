2f1fa711ed46b97caf430d74aaf4deaf9be6d550
Dailen Gunter <me@dailen.net>
2026-05-29 02:48:07 -0400

    Bump version to 2.8.1

--- Public/Start-SACInteractive.ps1
+++ Public/Start-SACInteractive.ps1
@@ -484,7 +484,7 @@ $command
         $borderLine = $V + (" " * $W) + $V
 
         # Top border + title
-        $title     = "  SURGICAL AUTODESK CLEANER  v2.8.0"
+        $title     = "  SURGICAL AUTODESK CLEANER  v2.8.1"
         $titlePad  = $title.PadLeft(($W + $title.Length) / 2)
         Write-Host "$TL$border$TR" -ForegroundColor DarkCyan
         Write-BoxLine -Text $titlePad -Color "Cyan"
